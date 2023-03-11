ea4a103353aba103f798bb4dffbe8c1251138294
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-11 11:32:46 -0300

    solo falta data de empresa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home/Home";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import About from "./Components/About/About";
-import Services from "./Components/Services/Services";
-import Contact from "./Components/Contact/Contact";
-import Providers from "./Components/Providers/Providers";
 
 
 
@@ -14,15 +9,9 @@ function App() {
 
     <BrowserRouter>
 
-      <Header />
-
       <Routes>
 
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/services" element={<Services />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/about-1" element={<Providers />} />
+        <Route exact path="/" element={<Header />} />
 
       </Routes>
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,64 +1,395 @@
-import React from "react";
+import { useState } from "react";
 import "./Header.css";
+import Home from "../Home/Home";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Providers from "../Providers/Providers";
+import Services from "../Services/Services";
 
 
 
-function NavBar(props) {
+function Header(props) {
+
+    const [pages, setPages] = useState("Home");
+    const [icon, setIcon] = useState("lineas");
 
     const onClick = (e) => {
 
         const DropDownMenu = document.querySelector(".dropdown_menu");
 
-        DropDownMenu.classList.toggle("open")
+        DropDownMenu.classList.toggle("open");
 
-        const isOpen = DropDownMenu.classList.contains("open")
+        if (icon === "lineas") {
+            setIcon("x");
+        } else {
+            setIcon("lineas");
+        }
 
     };
 
+    const onClickHome = (e) => {
+        setPages("Home");
+    };
+
+    const onClickAbout = (e) => {
+        setPages("About");
+    };
+
+    const onClickServices = (e) => {
+        setPages("Services");
+    };
+
+    const onClickContact = (e) => {
+        setPages("Contact");
+    };
+
+    const onClickProviders = (e) => {
+        setPages("Providers");
+    };
+
+    if (pages === "Home") {
+
+        return (
+
+            <div>
+
+                <header>
+
+                    <br></br>
+
+                    <div className="navbar">
+
+                        <div className="logo"><button onClick={onClickHome}>Lumina Crystal</button></div>
+
+                        <ul className="links">
+
+                            <li><button onClick={onClickAbout}>About</button></li>
+                            <li><button onClick={onClickServices}>Services</button></li>
+                            <li><button onClick={onClickContact}>Contact</button></li>
+                            <li><button onClick={onClickProviders}>Freight Payment Providers</button></li>
+
+                        </ul>
+
+                        <button onClick={onClickHome} className="action_btn">Home</button>
+
+                        <button className="toggle_btn" onClick={onClick}>
+                            {icon === "lineas" ?
+                                <ion-icon name="reorder-four-outline" size="large">
+                                </ion-icon> :
+                                <ion-icon name="close-outline" size="large">
+                                </ion-icon>
+                            }
+                        </button>
+
+                        < div className="dropdown_menu">
+
+                            <div>
+
+                                <li><button onClick={onClickAbout} className="action_btn_responsive">
+                                    About</button></li>
+                                <li><button onClick={onClickServices} className="action_btn_responsive">
+                                    Services</button></li>
+                                <li><button onClick={onClickContact} className="action_btn_responsive">
+                                    Contact</button></li>
+                                <li><button onClick={onClickProviders} className="action_btn_responsive">
+                                    Freight Payment</button></li>
+                                <li><button onClick={onClickHome} className="home_btn_responsive">
+                                    Home</button></li>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </header >
+
+                <div>
+
+                    <Home />
+
+                </div>
+
+            </div >
+
+        )
+
+    } else if (pages === "About") {
 
 
-    return (
+        return (
 
-        <header>
+            <div>
 
-            <br></br>
+                <header>
 
-            <div className="navbar">
+                    <br></br>
 
-                <div className="logo"><a href="/">Lumina Crystal</a></div>
+                    <div className="navbar">
 
-                <ul className="links">
+                        <div className="logo"><button onClick={onClickHome}>Lumina Crystal</button></div>
 
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/about-1">Freight Payment Providers</a></li>
+                        <ul className="links">
 
-                </ul>
+                            <li><button onClick={onClickAbout}>About</button></li>
+                            <li><button onClick={onClickServices}>Services</button></li>
+                            <li><button onClick={onClickContact}>Contact</button></li>
+                            <li><button onClick={onClickProviders}>Freight Payment Providers</button></li>
 
-                <a href="/" className="action_btn">Home</a>
+                        </ul>
 
-                <button className="toggle_btn" onClick={onClick}><ion-icon name="reorder-four-outline" size="large">
-                </ion-icon></button>
+                        <button onClick={onClickHome} className="action_btn">Home</button>
 
-                <div className="dropdown_menu">
+                        <button className="toggle_btn" onClick={onClick}>
+                            {icon === "lineas" ?
+                                <ion-icon name="reorder-four-outline" size="large">
+                                </ion-icon> :
+                                <ion-icon name="close-outline" size="large">
+                                </ion-icon>
+                            }
+                        </button>
 
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/about-1">Freight Payment</a></li>
-                    <li><a href="/" className="action_btn_responsive">Home</a></li>
+                        < div className="dropdown_menu">
+
+                            <div>
+
+                                <li><button onClick={onClickAbout} className="action_btn_responsive">
+                                    About</button></li>
+                                <li><button onClick={onClickServices} className="action_btn_responsive">
+                                    Services</button></li>
+                                <li><button onClick={onClickContact} className="action_btn_responsive">
+                                    Contact</button></li>
+                                <li><button onClick={onClickProviders} className="action_btn_responsive">
+                                    Freight Payment</button></li>
+                                <li><button onClick={onClickHome} className="home_btn_responsive">
+                                    Home</button></li>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </header >
+
+                <div>
+
+                    <About />
 
                 </div>
 
             </div>
 
-        </header>
+        )
 
-    )
+    } else if (pages === "Services") {
+
+
+        return (
+
+            <div>
+
+                <header>
+
+                    <br></br>
+
+                    <div className="navbar">
+
+                        <div className="logo"><button onClick={onClickHome}>Lumina Crystal</button></div>
+
+                        <ul className="links">
+
+                            <li><button onClick={onClickAbout}>About</button></li>
+                            <li><button onClick={onClickServices}>Services</button></li>
+                            <li><button onClick={onClickContact}>Contact</button></li>
+                            <li><button onClick={onClickProviders}>Freight Payment Providers</button></li>
+
+                        </ul>
+
+                        <button onClick={onClickHome} className="action_btn">Home</button>
+
+                        <button className="toggle_btn" onClick={onClick}>
+                            {icon === "lineas" ?
+                                <ion-icon name="reorder-four-outline" size="large">
+                                </ion-icon> :
+                                <ion-icon name="close-outline" size="large">
+                                </ion-icon>
+                            }
+                        </button>
+
+                        < div className="dropdown_menu">
+
+                            <div>
+
+                                <li><button onClick={onClickAbout} className="action_btn_responsive">
+                                    About</button></li>
+                                <li><button onClick={onClickServices} className="action_btn_responsive">
+                                    Services</button></li>
+                                <li><button onClick={onClickContact} className="action_btn_responsive">
+                                    Contact</button></li>
+                                <li><button onClick={onClickProviders} className="action_btn_responsive">
+                                    Freight Payment</button></li>
+                                <li><button onClick={onClickHome} className="home_btn_responsive">
+                                    Home</button></li>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </header >
+
+                <div>
+
+                    <Services />
+
+                </div>
+
+            </div>
+
+        )
+
+    } else if (pages === "Contact") {
+
+
+        return (
+
+            <div>
+
+                <header>
+
+                    <br></br>
+
+                    <div className="navbar">
+
+                        <div className="logo"><button onClick={onClickHome}>Lumina Crystal</button></div>
+
+                        <ul className="links">
+
+                            <li><button onClick={onClickAbout}>About</button></li>
+                            <li><button onClick={onClickServices}>Services</button></li>
+                            <li><button onClick={onClickContact}>Contact</button></li>
+                            <li><button onClick={onClickProviders}>Freight Payment Providers</button></li>
+
+                        </ul>
+
+                        <button onClick={onClickHome} className="action_btn">Home</button>
+
+                        <button className="toggle_btn" onClick={onClick}>
+                            {icon === "lineas" ?
+                                <ion-icon name="reorder-four-outline" size="large">
+                                </ion-icon> :
+                                <ion-icon name="close-outline" size="large">
+                                </ion-icon>
+                            }
+                        </button>
+
+                        < div className="dropdown_menu">
+
+                            <div>
+
+                                <li><button onClick={onClickAbout} className="action_btn_responsive">
+                                    About</button></li>
+                                <li><button onClick={onClickServices} className="action_btn_responsive">
+                                    Services</button></li>
+                                <li><button onClick={onClickContact} className="action_btn_responsive">
+                                    Contact</button></li>
+                                <li><button onClick={onClickProviders} className="action_btn_responsive">
+                                    Freight Payment</button></li>
+                                <li><button onClick={onClickHome} className="home_btn_responsive">
+                                    Home</button></li>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </header >
+
+                <div>
+
+                    <Contact />
+
+                </div>
+
+            </div>
+
+        )
+
+    } else if (pages === "Providers") {
+
+
+        return (
+
+            <div>
+
+                <header>
+
+                    <br></br>
+
+                    <div className="navbar">
+
+                        <div className="logo"><button onClick={onClickHome}>Lumina Crystal</button></div>
+
+                        <ul className="links">
+
+                            <li><button onClick={onClickAbout}>About</button></li>
+                            <li><button onClick={onClickServices}>Services</button></li>
+                            <li><button onClick={onClickContact}>Contact</button></li>
+                            <li><button onClick={onClickProviders}>Freight Payment Providers</button></li>
+
+                        </ul>
+
+                        <button onClick={onClickHome} className="action_btn">Home</button>
+
+                        <button className="toggle_btn" onClick={onClick}>
+                            {icon === "lineas" ?
+                                <ion-icon name="reorder-four-outline" size="large">
+                                </ion-icon> :
+                                <ion-icon name="close-outline" size="large">
+                                </ion-icon>
+                            }
+                        </button>
+
+                        < div className="dropdown_menu">
+
+                            <div>
+
+                                <li><button onClick={onClickAbout} className="action_btn_responsive">
+                                    About</button></li>
+                                <li><button onClick={onClickServices} className="action_btn_responsive">
+                                    Services</button></li>
+                                <li><button onClick={onClickContact} className="action_btn_responsive">
+                                    Contact</button></li>
+                                <li><button onClick={onClickProviders} className="action_btn_responsive">
+                                    Freight Payment</button></li>
+                                <li><button onClick={onClickHome} className="home_btn_responsive">
+                                    Home</button></li>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </header >
+
+                <div>
+
+                    <Providers />
+
+                </div>
+
+            </div>
+
+        )
+
+    };
 
 };
 
-export default NavBar;
+export default Header;
 
 

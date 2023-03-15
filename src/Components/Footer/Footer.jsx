05584515a20b95cb8fb1c2abdc96ebe79d167dc5
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-15 14:22:13 -0300

    href

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
                 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 
                     <div class="col-md-4 d-flex align-items-center">
-                        <a href="https://github.com/Molli1992?tab=repositories" target="_blank" class="mb-3 me-2 mb-md-0 text-white text-decoration-none lh-1">
+                        <a href="https://github.com/Molli1992?tab=repositories" target="_blank" rel="noreferrer" class="mb-3 me-2 mb-md-0 text-white text-decoration-none lh-1">
                             <ion-icon name="logo-github" size="large"></ion-icon>
                         </a>
                         <span class="mb-3 mb-md-0 text-white">&copy; 2022 Company, Inc</span>
@@ -23,12 +23,12 @@ function Footer() {
 
                     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
 
-                        <li class="ms-3"><a class="text-white" href="/" target="_blank">
+                        <li class="ms-3"><a class="text-white" href="/" target="_blank" rel="noreferrer">
                             <ion-icon name="logo-facebook" size="large"></ion-icon>
                         </a>
                         </li>
 
-                        <li class="ms-3"><a class="text-white" href="/" target="_blank">
+                        <li class="ms-3"><a class="text-white" href="/" target="_blank" rel="noreferrer">
                             <ion-icon name="logo-linkedin" size="large"></ion-icon>
                         </a></li>
 
